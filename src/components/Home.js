6d7fd27987476b52e243a06ac712baace5715bf7
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-02-16 01:39:39 +0900

    調整

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,11 +22,28 @@ class Home extends Component {
     const postList = posts.length ? (
       posts.map(post => {
         return(
-          <div className="post card" style={{"borderRadius":"0.6em"}} key={ post.pk }>
+          <div className="card" style={{"borderRadius":"0.6em"}} key={ post.pk }>
             <div className="card-content">
+            <details>
+              <summary style={{"userSelect":"none"}}>
               <span className="card-title w4" style={{"fontWeight":"400"}}>{ post.タイトル }</span>
-              <div className="right">hello</div>
+              <div className="" style={{margin:"1em auto","padding":"1em"}}>hello</div>
+              </summary>
+              <div>
+                <table>
+                <tbody>
+                <tr><td>日程</td><td>{ post.日程 }</td></tr>
+                <tr><td>ジャンル</td><td>{ post.ジャンル }</td></tr>
+                <tr><td>対象</td><td>{ post.対象 }</td></tr>
+                <tr><td>場所</td><td>{ post.実施場所 }</td></tr>
+                <tr><td>主催</td><td>{ post.主催 }</td></tr>
+                <tr><td></td><td></td></tr>
+                <tr><td></td><td></td></tr>
+                </tbody>
+                </table>
+              </div>
               <p>{ post.内容 }</p>
+              </details>
             </div>
           </div>
         )
