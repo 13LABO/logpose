69acd64a6c8a922476aa9292ddbc03d4d0518b22
gyawa24 <50992688+gyawa24@users.noreply.github.com>
2020-02-21 22:47:58 +0900

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -27,6 +27,22 @@ class About extends Component {
               I am convenient because I require little markup to use effectively.</p>
             </div>
           </div>
+          <div className="card grey lighten-5" style={{"borderRadius":"1em"}}>
+            <div className="card-content">
+              <span className="card-title">テスト投稿</span>
+              <span className="sub grey-text">2020/2/16</span>
+              <p style={{"margin":"1em 0"}}>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
+          <div className="card grey lighten-5" style={{"borderRadius":"1em"}}>
+            <div className="card-content">
+              <span className="card-title">テスト投稿</span>
+              <span className="sub grey-text">2020/2/16</span>
+              <p style={{"margin":"1em 0"}}>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+          </div>
         </div>
       </div>
      );
