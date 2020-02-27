57514a91635cfdd106f480943ffd9522fa211c8a
monkukui <monkukui1953@gmail.com>
2020-02-27 23:11:10 +0900

    Add: styled-components の参考となるコンポーネントの追加

--- src/components/TopPage/Top.js
+++ src/components/TopPage/Top.js
@@ -25,14 +25,13 @@ class Top extends Component {
 }
 
 const TopPageContainer = styled.div`
-  border: 1px solid #aaa;
+  border: 1px solid #aaa;  /* FIXME 後でとる */
   width: 375px;  /* スマホの横幅ってどんなもんだろう？ px で設定する必要あるか？ */
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   margin: auto;
-  color: red; /* this is just test */
 `;
  
 export default Top;
--- src/components/TopPage/components/Heading.js
+++ src/components/TopPage/components/Heading.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
+import styled from 'styled-components';
+
+const data = "LOGPOSEで\n北海道の就活イベントを\n探そう";
+
 class Heading extends Component {
   state = {  }
   render() { 
     return (
       <div>
-        <h1>就活イベント，探そうぜ！</h1>
+        <HeadingContainer>
+          <HeadingText>
+            {data}
+          </HeadingText>
+        </HeadingContainer>
       </div>
     );
   }
 }
+
+const HeadingText = styled.div`
+  white-space: pre-line;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+`
+
+const HeadingContainer = styled.div`
+  border: 1px solid #aaa;  /* FIXME 後でとる */
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10px;
+  background-color: black;
+`
  
 export default Heading;
