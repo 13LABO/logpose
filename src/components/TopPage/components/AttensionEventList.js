c31b2e9faa636e1a1e95507af1a3713f0576b33b
monkukui <monkukui1953@gmail.com>
2020-03-03 19:41:32 +0900

    Add: react-swaipable-views

--- src/components/TopPage/components/AttensionEventList.js
+++ src/components/TopPage/components/AttensionEventList.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import SwipeableViews from 'react-swipeable-views';
 
 const topBarText = "おすすめ";
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    background: '#FEA900',
+  },
+  slide2: {
+    background: '#B3DC4A',
+  },
+  slide3: {
+    background: '#6AC0FF',
+  },
+};
 
 export default function AttensionEventList() {
 
@@ -12,6 +29,19 @@ export default function AttensionEventList() {
           {topBarText}
         </TopBarTextContainer>
       </TopBarWrapper>
+
+      <SwipeableViews>
+        <div style={Object.assign({}, styles.slide, styles.slide1)}>
+          slide n°1
+        </div>
+        <div style={Object.assign({}, styles.slide, styles.slide2)}>
+          slide n°2
+        </div>
+        <div style={Object.assign({}, styles.slide, styles.slide3)}>
+          slide n°3
+        </div>
+      </SwipeableViews>
+
     </> 
   );
 }
