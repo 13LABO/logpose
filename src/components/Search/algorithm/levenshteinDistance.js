c1ff6aa31f42bcef2d9a7ff68a098e9f9a4d5897
monkukui <monkukui1953@gmail.com>
2020-03-06 23:47:43 +0900

    Modify: levenshteinDistance

--- src/components/Search/algorithm/levenshteinDistance.js
+++ src/components/Search/algorithm/levenshteinDistance.js
@@ -1,20 +1,20 @@
 export default function levenshteinDistance( str1, str2 ) {
-    var x = str1.length;
-    var y = str2.length;
+    let x = str1.length;
+    let y = str2.length;
 
-    var d = [];
-    for( var i = 0; i <= x; i++ ) {
+    let d = [];
+    for( let i = 0; i <= x; i++ ) {
         d[i] = [];
         d[i][0] = i;
     }
-    for( var i = 0; i <= y; i++ ) {
+    for( let i = 0; i <= y; i++ ) {
         d[0][i] = i;
     }
 
     var cost = 0;
-    for( var i = 1; i <= x; i++ ) {
-        for( var j = 1; j <= y; j++ ) {
-            cost = str1[i - 1] == str2[j - 1] ? 0 : 1;
+    for( let i = 1; i <= x; i++ ) {
+        for( let j = 1; j <= y; j++ ) {
+            cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
 
             d[i][j] = Math.min( d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost );
         }
